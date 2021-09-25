3a3733b65a49c29140cc1c490d0dbc1739953018
newhar <gtqh2yj@gmail.com>
2021-09-25 23:33:40 +0900

    first commit

---
+++ src/main/java/com/clonecoin/walletwrite/WalletwriteApplication.java
@@ -0,0 +1,15 @@
+package com.clonecoin.walletwrite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class WalletwriteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletwriteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clonecoin/walletwrite/WalletwriteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonecoin.walletwrite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletwriteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
